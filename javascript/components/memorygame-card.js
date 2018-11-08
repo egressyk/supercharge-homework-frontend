8cda91562b3581f16d62660befe44072ce462a00
egressyk@gmail.com <Rinoa1986>
2018-11-08 11:50:16 +0100

    Card front images
    Implemented functionality to attach relevant front images to cards.

--- javascript/components/memorygame-card.js
+++ javascript/components/memorygame-card.js
@@ -1,13 +1,27 @@
 class MemoryCard extends HTMLElement {
+    constructor() {
+        super();
+        this._frontImageUrl = '';
+    }
+
     get faceUP() {
         return this.hasAttribute('faceUp');
+    }
+
+    get frontImageUrl() {
+        return this._frontImageUrl;
+    }
+
+    set frontImageUrl(imgUrl) {
+        this._frontImageUrl = imgUrl;
     }
 
     connectedCallback() {
         this.classList.add('card');
         this.innerHTML = `
-                <div class="back">Back</div>
-                <div class="front">Front</div>`;
+                <div class="back"></div>
+                <div class="front" style="background-image: url(${this._frontImageUrl})">
+                </div>`;
     }
 
     flip() {
