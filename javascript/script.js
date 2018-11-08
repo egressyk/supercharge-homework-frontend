fe63fc7bddb5cf90bb38c78338f26f203ce1b385
egressyk@gmail.com <Rinoa1986>
2018-11-08 11:08:11 +0100

    Game board initializer
    Added game board initializer function, that populates the board with cards attaching flip-on-clik event to them.

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,18 @@
 import './components/memorygame-card.js';
 
-const card = document.createElement('memorygame-card');
-document.querySelector('body').appendChild(card);
-card.addEventListener('click', function () {
-    this.flip();
-});
+const deckOfCards = [];
+
+const cardBoard = document.querySelector('#game-board');
+
+function initBoard(numberOfPairs = 2) {
+    for (let i = 1; i <= numberOfPairs * 2; i++) {
+        const newCard = document.createElement('memorygame-card');
+        newCard.addEventListener('click', function () {
+            this.flip();
+        });
+        deckOfCards.push(newCard);
+        cardBoard.appendChild(newCard);
+    }
+}
+
+initBoard(5);
